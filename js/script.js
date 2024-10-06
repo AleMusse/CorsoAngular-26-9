560a3e6314280a0cbe30e8c0b8b6ec5e7b8f05d3
AleMusse <alessio.tognetto@gmail.com>
2024-10-06 21:53:57 +0200

    ready x

--- js/script.js
+++ js/script.js
@@ -1,96 +1,4 @@
 
-// fetch("http://localhost:3001/games")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const gameContainer = document.querySelector(".container div");
-
-//     let dataC = data.result;
-//     for (let game of dataC) {
-//       gameContainer.innerHTML += `
-//         <div class="card h-100">
-//           <img src="${game.image}" class="card-img-top" alt="${game.name}" />
-//           <div class="card-body d-flex flex-column justify-content-between">
-//             <h2 class="card-title fs-5">${game.name}</h2>
-//             <h3 class="fs-6">${game.publisher}</h3>
-//             <p class="card-text">${game.description}</p>
-//             <p class="card-rating">{this.getGameStarsRating(game.id)}</p>
-//             <p class="card-details">
-//               <span class="badge text-bg-info">New</span>
-//               <span class="badge text-bg-light">${game.genre}</span>
-//               <span class="badge text-bg-light">${game.developer}</span>
-//             </p>
-//             <div class="btn btn-primary btn--view-more">View More details</div>
-//           </div>
-//         </div>`
-//         ;
-
-//       let modal = document.querySelector(".modal");
-//       let buttonView = document.querySelectorAll(".btn--view-more");
-
-      
-//       for (let bottoni of buttonView) {
-//         bottoni.addEventListener("click", () => {
-//           console.log("hai bestemmiato");
-//           modal.innerHTML = `
-//                   <div class="modal-dialog d-flex m-auto">
-//               <div class="modal-content m-auto">
-//                 <div class="modal-header">
-//                   <h5 class="modal-title">Selected Game</h5>
-//                 </div>
-//                 <div class="modal-body p-0">
-//                   <div class="container-fluid">
-//                     <div class="row my-3">
-//                       <div class="col-2">
-//                         <img src="${game.image}" class="modal-img img-fluid" alt="..." />  
-//                       </div>
-//                       <div class="col">
-//                         <p class="modal-details">
-//                           <span class="badge text-bg-info">New</span>
-//                           <span class="modal-genre badge text-bg-light">${game.genre}</span>
-//                           <span class="modal-developer badge text-bg-light">${game.developer}</span>
-//                         </p>
-//                         <div class="modal-body">
-//                           <h2 class="modal-name fs-5">${game.name}</h2>
-//                           <h3 class="modal-publisher fs-6">${game.publisher}</h3>
-
-//                           <p class="modal-summary">${game.summary}</p>
-
-//                         </div>
-//                       </div>
-//                     </div>
-//                   </div>
-
-
-//                   <div class="modal-footer">
-//                     <button type="button" class="btn btn-secondary modal-close" data-bs-dismiss="modal">
-//                       Close
-//                     </button>
-//                   </div>
-//                 </div>
-//               </div>`
-//             ;
-//           modal.style.display = "block";
-
-//           let close = document.querySelector(".btn-secondary")
-//           close.addEventListener("click", () => {
-//             modal.style.display = "none"
-//           })
-
-
-//         }); //click button
-//       }// for button
-//     }// for 1
-//   }) // .then
-//   .catch((error) => {
-//     console.log("Error fetching data:", error);
-//   });
-
-
-
-
-
-
-
 fetch("http://localhost:3001/games")
   .then((response) => response.json())
   .then((data) => {
@@ -118,13 +26,11 @@ fetch("http://localhost:3001/games")
       `;
 
 
-      // {this.getGameStarsRating(game.id)}
-
       gameContainer.appendChild(gameCard);
 
       const viewMoreButton = gameCard.querySelector(".btn--view-more");
       viewMoreButton.addEventListener("click", function() {
-        const clickedGame = game; // Capture the clicked game object
+        const clickedGame = game; 
 
         const modal = document.querySelector(".modal");
         modal.innerHTML = `
